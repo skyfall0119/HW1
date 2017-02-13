12dae1a656043ce9209e4420895a1a63bb896c55
Jay Kim <skyfall0119@gmail.com>
2017-02-13 17:10:36 -0600

    view 2 done. working on param passing to final result

--- www/js/app.js
+++ www/js/app.js
@@ -30,8 +30,8 @@ var myApp = angular.module('myApp', ['ionic'])
     .state( 'result', {
       url:"/result",
       params: {
-        'q1_answer_param' : null,
-        'q2_answer_param' : null
+        'q1_result_param' : null,
+        'q2_result_param' : null
       },
       templateUrl:"templates/view4.html",
       controller: 'resultCtrl'
@@ -42,32 +42,92 @@ var myApp = angular.module('myApp', ['ionic'])
 
 
 // in the start page, nothing really happens. just clicking on start to proceed
-myApp.controller('startCtrl', function($scope, $state, $stateParams) {
-
-
+myApp.controller('startCtrl', function($scope, $state) {
   // start button pressed
   $scope.start = function () {
     $state.go("q1")
   };
 })
 
-myApp.controller('q1Ctrl', function($scope, $state, $stateParams) {
+myApp.controller('q1Ctrl', function($scope, $state, $ionicLoading) {
+  $scope.hour=null;
+  $scope.minute=null;
+  var result = null;
+
 
 
   $scope.check = function() {
+    // get time from device
+    var time = new Date();
+    var h = time.getHours();
+    var m = time.getMinutes();
+
+    if ($scope.hour == null || $scope.minute == null ) {
+      $ionicLoading.show({ template: 'put your answer.', noBackdrop: true, duration: 1000 });
+    } else if ($scope.hour != h || $scope.minute != m ) {
+      result = false;
+    } else {
+      result = true;
+    }
+
+    if (result != null) {
+      $state.go("q2", {
+        'q1_result_param' : result
+      })
+    }
+  };
+
+})
+
+myApp.controller('q2Ctrl', function($scope, $state) {
+
+  var random = Math.floor((Math.random() * 10) + 1);
+  var ans = "";
+  var result = false;
 
 
-
-
+  if (random >= 1 && random <= 5) {
+    ans = "ocean";
+  } else {
+    ans = "mountain";
   }
+
+
+
+  $scope.ocean = function() {
+      if (ans == "ocean") {
+        result = true;
+      }
+    $state.go("result", {
+      'q2_result_param' : result
+    })
+  }
+
+  $scope.mountain = function () {
+    if (ans == "mountain") {
+      result = true;
+    }
+    $state.go("result", {
+      'q2_result_param' : result
+    })
+  }
+
+
 })
 
-myApp.controller('q2Ctrl', function($scope, $state, $stateParams) {
 
-
-
-})
 myApp.controller('resultCtrl', function($scope, $state, $stateParams) {
+
+
+  if ($stateParams.q1_result_param) document.getElementById("q1Result").innerHTML = "correct";
+  else document.getElementById("q1Result").innerHTML = "wrong";
+
+
+  if ($stateParams.q2_result_param) document.getElementById("q2Result").innerHTML = "correct";
+  else document.getElementById("q2Result").innerHTML = "wrong";
+
+
+
 
 
 
